f76c0cf90f718e5671297b1b22da0c0bf768c1c2
Vivien Didelot <vivien.didelot@gmail.com>
2010-05-10 18:08:44 +0200

    condom.rb is almost done

--- bin/condom.rb
+++ bin/condom.rb
@@ -5,33 +5,24 @@ require 'erb'
 require 'optparse'
 
 version = 1.6
-err = 65
-$script = File.basename $0
-date = Time.now.strftime("%d %B %Y")
+$today = Time.now.strftime("%d %B %Y")
 
+user = Etc.getpwnam(Etc.getlogin)['gecos'].split(',')[0]
+user = Etc.getlogin if user.nil?
 $options = {
-    :outputdir     => Dir.getwd,
-    :math?         => false,
-    :listings?     => false,
-    :fancyhdr?     => false,
-    :pdf?          => false,
-    :graphics?     => false,
-    :listings_path => 'src/',
-    :listings_conf => 'lst_conf',
-    :graphics_path => 'fig/',
-    :graphics_sum  => 'fig.tex.txt',
-    :texfilename   => 'main',
-    :filename      => 'main',
-    :makefile      => 'Makefile',
-    :author        => Etc.getpwnam(Etc.getlogin)['gecos'].split(',')[0],
-    :title         => 'Document \LaTeX',
-    :date          => '\today',
-    :class         => 'article',
-    :flyleaf       => 'flyleaf',
-    :inc_path      => 'inc/',
-    :packages      => []
+    :outputdir => Dir.getwd,
+    :listings? => false,
+    :fancyhdr? => false,
+    :graphics? => false,
+    :math?     => false,
+    :pdf?      => false,
+    :filename  => 'main',
+    :author    => user,
+    :title     => 'Document \LaTeX',
+    :date      => '\today',
+    :class     => 'article',
+    :packages  => []
 }
-$options[:author] = Etc.getlogin if $options[:author].nil?
 
 class String
     def >> str
@@ -40,10 +31,11 @@ class String
 end
 
 def echo str
-    puts $script >> ": " << str
+    puts "condom: " << str
 end
 
 def touch file, template
+    echo "creation de #{file}"
     content = ERB.new(template).result
     f = File.open(file, 'w')
     f.write content
@@ -58,54 +50,68 @@ def print_values
     printf("%-10s%-26s", "-m", "math")
     puts $options[:math?] ? "oui" : "non"
     printf("%-10s%-26s", "-l", "listings")
-    puts $options[:listings?] ? "oui, dossier #{$options[:outputdir]}/#{$options[:listings_path]}" : "non"
+    puts $options[:listings?] ? "oui, dossier #{$options[:outputdir]}/src" : "non"
     printf("%-10s%-26s", "-f", "fancyhdr")
     puts $options[:fancyhdr?] ? "oui" : "non"
     printf("%-10s%-26s", "-p", "config pdf")
     puts $options[:pdf?] ? "oui" : "non"
     printf("%-10s%-26s", "-i", "images")
-    puts $options[:graphics?] ? "oui, dossier #{$options[:outputdir]}/#{$options[:graphics_path]}" : "non"
+    puts $options[:graphics?] ? "oui, dossier #{$options[:outputdir]}/fig" : "non"
     printf("%-10s%-26s%s\n", "-n", "nom du fichier", $options[:filename])
     printf("%-10s%-26s%s\n", "-a", "auteur", $options[:author])
     printf("%-10s%-26s%s\n", "-t", "titre", $options[:title])
     printf("%-10s%-26s%s", "-d", "date", $options[:date])
-    puts $options[:date] == '\today' ? " (" + date + ")" : ""
+    puts $options[:date] == '\today' ? " (" + $today + ")" : ""
     printf("%-10s%-26s%s\n", "-c", "classe", $options[:class])
     printf("%-10s%-26s", "-P", "paquets supplementaires")
     puts $options[:packages].empty? ? "aucun" : "oui: " + $options[:packages].join(", ")
 end
 
-op = OptionParser.new do |o|
-    o.on("-m", "--math",     "Paquets mathematiques")     { $options[:math?] = true }
-    o.on("-l", "--listings", "Paquet Listings")           { $options[:listings?] = true }
-    o.on("-f", "--fancyhdr", "Paquet Fancyhdr")           { $options[:fancyhdr?] = true }
-    o.on("-p", "--pdf",      "Configuration pdf")         { $options[:pdf?] = true }
-    o.on("-g", "--graphics", "Paquets pour images")       { $options[:graphics?] = true }
-    o.on("-n", "--filename", "Definir un nom de fichier") { |v| $options[:filename] = v }
-    o.on("-a", "--author",   "Definir l'auteur")          { |v| $options[:author] = v }
-    o.on("-t", "--title",    "Definir le titre")          { |v| $options[:title] = v }
-    o.on("-d", "--date",     "Definir la date")           { |v| $options[:date] = v }
-    o.on("-c", "--class",    "Definir la classe")         { |v| $options[:class] = v }
-    o.on("-P", "--package",  "Ajouter un paquet")         { |v| $options[:packages].push v }
-    o.on("-v", "--version",  "Afficher la version")       { echo "version #{version}" ; exit }
+ARGV.options do |o|
+    o.on("-m", "--math", "Paquets mathematiques",
+        $options[:math?] ? "oui" : "non") { $options[:math?] = true }
+    o.on("-l", "--listings", "Paquet Listings",
+        $options[:listings?] ? "oui" : "non") { $options[:listings?] = true }
+    o.on("-f", "--fancyhdr", "Paquet Fancyhdr",
+        $options[:fancyhdr?] ? "oui" : "non") { $options[:fancyhdr?] = true }
+    o.on("-p", "--pdf", "Configuration pdf",
+        $options[:pdf?] ? "oui" : "non") { $options[:pdf?] = true }
+    o.on("-g", "--graphics", "Paquets pour images",
+        $options[:graphics?] ? "oui" : "non") { $options[:graphics?] = true }
+    o.on("-n", "--filename=FILENAME", String, "Definir un nom de fichier",
+        $options[:filename]) { |v| $options[:filename] = v }
+    o.on("-a", "--author=AUTHOR", String, "Definir l'auteur",
+        $options[:author]) { |v| $options[:author] = v }
+    o.on("-t", "--title=TITLE", String, "Definir le titre",
+        $options[:title]) { |v| $options[:title] = v }
+    o.on("-d", "--date=DATE", String, "Definir la date",
+        $options[:date] + ($options[:date] == '\today' ? " (" + $today + ")" : "")) { |v| $options[:date] = v }
+    o.on("-c", "--class=DATE", String, "Definir la classe",
+        $options[:class]) { |v| $options[:class] = v }
+    o.on("-P", "--package=PACKAGE", String, "Ajouter un paquet",
+        $options[:packages].empty? ? "aucun" : "oui: " + $options[:packages].join(", ")) { |v| $options[:packages].push v }
+    o.on("-v", "--version",  "Afficher la version",
+        version.to_s) { echo "version #{version}" ; exit }
+
+    o.on_tail("destination : " << $options[:outputdir] + ($options[:outputdir] == Dir.getwd ? " (dossier courant)" : ""))
 end.parse!
 
 if ARGV.length > 1
     echo "Mauvaise syntaxe."
     exit
-elsif ARGV.length == 1 and ARGV.first != "."
-    $options[:outputdir] = ARGV.first.chomp("/")
+elsif ARGV.length == 1
+    $options[:outputdir] = (ARGV.first == ".") ? Dir.getwd : ARGV.first.chomp("/")
     Dir.getwd >> "/" >> $options[:outputdir] unless $options[:outputdir].chars.first == "/"
 end
 
 # demander confirmation
-print_values
+#print_values
+#TODO actualiser ARGV.options
 print "\nVoulez-vous continuer [Y/n/h] ? "
 case STDIN.gets.strip
 when 'Y', 'y', ''
 when 'h'
-    #TODO
-    puts "help"
+    puts ARGV.options
     exit
 else
     echo "Abandon."
@@ -113,28 +119,25 @@ else
 end
 
 # verifier la destination
-unless File.directory? $options[:outputdir]
+if File.directory? $options[:outputdir]
+    Dir.chdir $options[:outputdir] unless $options[:outputdir] == Dir.getwd
+    if File.exist? "main.tex"
+        echo "#{$options[:outputdir]}: il existe deja un fichier main.tex"
+        exit
+    elsif File.exist? "Makefile"
+        echo "#{$options[:outputdir]}: il existe deja un Makefile"
+        exit
+    end
+else
     Dir.mkdir $options[:outputdir]
     Dir.chdir $options[:outputdir]
-else
-    Dir.chdir $options[:outputdir] unless $options[:outputdir] == Dir.getwd
-    if File.exist? "#{$options[:texfilename]}.tex"
-        puts "#{$options[:outputdir]}: il existe deja un fichier #{$options[:texfilename]}.tex"
-        exit err
-    elsif File.exist? $options[:makefile]
-        puts "#{$options[:outputdir]}: il existe deja un #{$options[:makefile]}"
-        exit err
-    end
 end
 
-Dir.mkdir $options[:inc_path]
-Dir.mkdir $options[:listings_path] if $options[:listings?]
-Dir.mkdir $options[:graphics_path] if $options[:graphics?]
-
-# creation du makefile
+# creation des templates
+# makefile
 makefile_tpl = %q{
-# <%= $options[:makefile] %> genere le <%= date %>
-# par <%= $options[:author] %> a l'aide du script <%= $script %>
+# Makefile genere le <%= $today %>
+# par <%= $options[:author] %> a l'aide du script condom
 
 FINAL_FILENAME=<%= $options[:filename] %>
 FILENAME=main
@@ -149,7 +152,9 @@ ifeq ($(RUBBER),)
 else
 	@$(RUBBER) -d $(FILENAME).tex
 endif
+ifneq ($(FILENAME),$(FINAL_FILENAME))
 	@mv $(FILENAME).pdf $(FINAL_FILENAME).pdf
+endif
 
 view: all
 	@$(VIEWER) $(FINAL_FILENAME).pdf
@@ -163,57 +168,6 @@ clean:
 archive: all clean
 	@tar -cf $(FINAL_FILENAME).tar *
 	@echo "archived in $(FINAL_FILENAME).tar"
-}.strip
-
-file = $options[:makefile]
-touch file, makefile_tpl
-echo "creation du #{file}"
-
-# liste des couleurs
-colors_tpl = %q{
-%         Colors    Bright Colors
-% Black   #0F2130   #B0B3B9
-% Red     #D22613   #F96795
-% Green   #7CDE53   #A0F2A0
-% Yellow  #EBE645   #F4EF82
-% Blue    #2C9ADE   #A3FEFE
-% Orange  #FFA705   #F1B356
-% Cyan    #8A95A7   #B0C3DA
-% White   #F8F8F8   #FFFFFF
-
-\definecolor{myblack}{HTML}{0F2130}
-\definecolor{myred}{HTML}{D22613}
-\definecolor{mygreen}{HTML}{7CDE53}
-\definecolor{myyellow}{HTML}{EBE645}
-\definecolor{myblue}{HTML}{2C9ADE}
-\definecolor{myorange}{HTML}{FFA705}
-\definecolor{mycyan}{HTML}{8A95A7}
-\definecolor{mywhite}{HTML}{F8F8F8}
-\definecolor{mygrey}{HTML}{555753}
-}.strip
-
-# liste des commandes perso
-commands_tpl = %q{
-% raccourcis
-\newcommand{\Cad}{C'est-à-dire~}
-\newcommand{\cad}{c'est-à-dire~}
-\newcommand{\pe}{peut-être~}
-
-\newcommand{\todo}[1]{\bigskip \colorbox{myyellow}{\textcolor{mygrey}{\textsf{\textbf{TODO} #1 }}} \bigskip}
-
-<% if opts[:pdf?] %>
-\newcommand{\email}[1]{\href{mailto:#1}{\textsf{<#1>}}}
-<% end %>
-
-<% if $options[:graphics?] %>
-% commande pour afficher un lien vers une image
-\newcommand{\figref}[1]{\textsc{Fig.}~\ref{#1} (p.~\pageref{#1})}
-<% end %>
-
-<% if $options[:listings?] %>
-% commande pour afficher le lien vers un listing :
-\newcommand{\lstref}[1]{{\footnotesize Listing~\ref{#1}, p.~\pageref{#1}}}
-<% end %>
 }.strip
 
 # liste des packages
@@ -258,11 +212,65 @@ packages_tpl = %q{
 <% $options[:packages].each { |p| puts '\usepackage{' << p << '}' } %>
 }.strip
 
-# creation du fichier de config de listings si besoin
-if $options[:listings?]
+# liste des couleurs
+colors_tpl = %q{
+%         Colors    Bright Colors
+% Black   #0F2130   #B0B3B9
+% Red     #D22613   #F96795
+% Green   #7CDE53   #A0F2A0
+% Yellow  #EBE645   #F4EF82
+% Blue    #2C9ADE   #A3FEFE
+% Orange  #FFA705   #F1B356
+% Cyan    #8A95A7   #B0C3DA
+% White   #F8F8F8   #FFFFFF
+
+\definecolor{myblack}{HTML}{0F2130}
+\definecolor{myred}{HTML}{D22613}
+\definecolor{mygreen}{HTML}{7CDE53}
+\definecolor{myyellow}{HTML}{EBE645}
+\definecolor{myblue}{HTML}{2C9ADE}
+\definecolor{myorange}{HTML}{FFA705}
+\definecolor{mycyan}{HTML}{8A95A7}
+\definecolor{mywhite}{HTML}{F8F8F8}
+\definecolor{mygrey}{HTML}{555753}
+}.strip
+
+# liste des commandes perso
+commands_tpl = %q{
+% raccourcis
+\newcommand{\Cad}{C'est-à-dire~}
+\newcommand{\cad}{c'est-à-dire~}
+\newcommand{\pe}{peut-être~}
+
+\newcommand{\todo}[1]{\bigskip \colorbox{myyellow}{\textcolor{mygrey}{\textsf{\textbf{TODO} #1 }}} \bigskip}
+
+<% if $options[:pdf?] %>
+\newcommand{\email}[1]{\href{mailto:#1}{\textsf{<#1>}}}
+<% end %>
+
+<% if $options[:graphics?] %>
+% commande pour afficher un lien vers une image
+\newcommand{\figref}[1]{\textsc{Fig.}~\ref{#1} (p.~\pageref{#1})}
+<% end %>
+
+<% if $options[:listings?] %>
+% commande pour afficher le lien vers un listing :
+\newcommand{\lstref}[1]{{\footnotesize Listing~\ref{#1}, p.~\pageref{#1}}}
+<% end %>
+
+<% if $options[:class] == "report" %>
+\newcommand{\unnumchap}[1]{%
+    \chapter*{#1}%
+    \addcontentsline{toc}{chapter}{#1}%
+    \chaptermark{#1}%
+}
+<% end %>
+}.strip
+
+# fichier de config pour listings
 listings_tpl = %q{
-% fichier de config du paquet listings genere le <%= date %>
-% par <%= $options[:author] %> a l'aide du script <%= $script %>
+% fichier de config du paquet listings genere le <%= $today %>
+% par <%= $options[:author] %> a l'aide du script condom
 %
 % /!\ ne pas utiliser de caracteres accentues dans les sources (ne gere pas l'utf-8)
 % pour remplacer les lettres accentues : sed -i -e "y/ÉÈÊÇÀÔéèçàôîêûùï/EEECAOeecaoieuui/" fichier
@@ -365,16 +373,10 @@ listings_tpl = %q{
 %\lstinputlisting[caption={Legende}, label=lst:label]{emplacement}
 }.strip
 
-    file = "#{$options[:listings_conf]}.tex"
-    touch file, listings_tpl
-    echo "creation du fichier #{file}"
-end
-
-# creation de la page de garde personnalisee si besoin
-if $options[:class] == "report"
+# page de garde personnalisee
 flyleaf_tpl = %q{
-% page de garde personnalisee generee le <%= date %>
-% par <%= $options[:author] %> a l'aide du script <%= $script %>
+% page de garde personnalisee generee le <%= $today %>
+% par <%= $options[:author] %> a l'aide du script condom
 % voir http://zoonek.free.fr/LaTeX/LaTeX_samples_title/0.html pour d'autres modeles
 
 \makeatletter
@@ -429,16 +431,10 @@ flyleaf_tpl = %q{
 \makeatother
 }.strip
 
-    file = "#{$options[:flyleaf]}.tex"
-    touch file, flyleaf_tpl
-    echo "creation du fichier #{file}"
-end
-
-# creation de l'aide-memoire pour les images si besoin
-if $options[:graphics?]
+# aide-memoire pour les images
 graphics_sum_tpl = %q{
 % aide-memoire pour l'insertion d'image sous LaTeX genere le <%= Time.now %>
-% par <%= $options[:author] %> a l'aide du script <%= $script %>
+% par <%= $options[:author] %> a l'aide du script condom
 
 % [!h] est facultatif, ca permet d'indiquer la position de la figure (si possible !)
 % ! = forcer la position
@@ -495,16 +491,12 @@ graphics_sum_tpl = %q{
 \end{figure}
 }.strip
 
-    file = $options[:graphics_sum]
-    touch file, graphics_sum_tpl
-    echo "creation du fichier #{file}"
-end
-
-# creation du fichier tex
-if opts[:class] == "beamer"
-texfilename_tpl = %q{
-% squelette LaTeX genere le <%= date %>
-% par <%= $options[:author] %> a l'aide du script <%= $script %>
+# fichier tex principal
+main_tpl = ""
+if $options[:class] == "beamer"
+main_tpl = %q{
+% squelette LaTeX genere le <%= $today %>
+% par <%= $options[:author] %> a l'aide du script condom
 
 %%%%%%%%%%%%%
 % PREAMBULE %
@@ -513,15 +505,18 @@ texfilename_tpl = %q{
 % http://latex-beamer.sourceforge.net/
 % http://www.tex.ac.uk/tex-archive/macros/latex/contrib/beamer/doc/beameruserguide.pdf
 
-\documentclass{<%= $options[:class] %>}
+\documentclass{beamer}
 \usetheme{Warsaw}
 
-<% if $options[:graphics?] %>
-\graphicspath{{<%= $options[:graphics_path] %>}}                 % chemins vers les images
+\input{inc/packages}
+\input{inc/colors}
+\input{inc/commands}
+<% if $options[:listings?] %>
+\input{inc/lst-conf}                        % fichier de config du paquet listings
 <% end %>
 
-<% if $options[:listings?] %>
-\include{<%= $options[:listings_conf] %>}                        % fichier de config du paquet listings
+<% if $options[:graphics?] %>
+\graphicspath{{fig}}                 % chemins vers les images
 <% end %>
 
 % informations du document
@@ -552,22 +547,53 @@ texfilename_tpl = %q{
 %EOF
 }.strip
 else
-texfilename_tpl = %q{
-% squelette LaTeX genere le <%= date %>
-% par <%= $options[:author] %> a l'aide du script <%= $script %>
+main_tpl = %q{
+% squelette LaTeX genere le <%= $today %>
+% par <%= $options[:author] %> a l'aide du script condom
 
 %%%%%%%%%%%%%
 % PREAMBULE %
 %%%%%%%%%%%%%
 
 \documentclass[a4paper, 11pt]{<%= $options[:class] %>}           % format general
-\geometry{top=2.5cm, bottom=2.5cm, left=2cm, right=2cm}
 
-<% if $options[:graphics?] %>
-\graphicspath{{<%= $options[:graphics_path] %>}}                 % chemins vers les images
+\input{inc/packages}
+\input{inc/colors}
+\input{inc/commands}
+<% if $options[:listings?] %>
+\input{inc/lst-conf}                        % fichier de config du paquet listings
 <% end %>
 
-<% $options[:fancyhdr?] %>
+\geometry{top=2.5cm, bottom=2.5cm, left=2cm, right=2cm}
+<% if $options[:graphics?] %>
+\graphicspath{{fig}}                 % chemins vers les images
+<% end %>
+
+% informations du document
+\author{<%= $options[:author] %>}
+\date{Le <%= $options[:date] %>}
+\title{<%= $options[:title] %>}
+
+<% if $options[:class] == "report" %>
+%\input{inc/flyleaf.tex} % page de garde personnalisee
+%\location{}
+%\blurb{}
+<% end %>
+
+<% if $options[:pdf?] %>
+\hypersetup{%
+    pdftitle    = {<%= $options[:title] %>},
+    pdfauthor   = {<%= $options[:author] %>}
+    pdfcreator  = {Texlive},
+    pdfproducer = {Texlive},
+    colorlinks  = true,
+    linkcolor   = black,
+    citecolor   = black,
+    urlcolor    = black
+}                                                   % informations du pdf
+<% end %>
+
+<% if $options[:fancyhdr?] %>
 \pagestyle{fancy}
 \lhead{<%= $options[:title] %>}
 \chead{}
@@ -584,40 +610,8 @@ texfilename_tpl = %q{
 }
 <% end %>
 
-<% $options[:pdf?] %>
-\hypersetup{%
-    pdftitle    = {<%= $options[:title] %>},
-    pdfauthor   = {<%= $options[:author] %>}
-    pdfcreator  = {Texlive},
-    pdfproducer = {Texlive},
-    colorlinks  = true,
-    linkcolor   = black,
-    citecolor   = black,
-    urlcolor    = black
-}                                                   % informations du pdf
-<% end %>
-
-<% if $options[:listings?] %>
-\include{<%= $options[:listings_conf] %>}                        % fichier de config du paquet listings
-<% end %>
-
-% informations du document
-\author{<%= $options[:author] %>}
-\date{Le <%= $options[:date] %>}
-\title{<%= $options[:title] %>}
-
 <% if $options[:class] == "report" %>
-%\include{<%= $options[:flyleaf] %>} % page de garde personnalisee
-%\location{}
-%\blurb{}
-
 \renewcommand{\thesection}{\Roman{part}.\arabic{section}} % redefinir la numerotation des sections (ex: I.2)
-
-\newcommand{\unnumchap}[1]{%
-    \chapter*{#1}%
-    \addcontentsline{toc}{chapter}{#1}%
-    \chaptermark{#1}%
-}
 <% end %>
 
 \makeatletter
@@ -642,7 +636,7 @@ texfilename_tpl = %q{
 
 \tableofcontents % table des matieres
 
-<% $options[:listings?] %>
+<% if $options[:listings?] %>
 \lstlistoflistings % tables des listings
 <% end %>
 
@@ -657,9 +651,21 @@ texfilename_tpl = %q{
 }.strip
 end
 
-file = "#{$options[:texfilename]}.tex"
-touch file, texfilename_tpl
-echo "creation du fichier #{file}"
+# creation des fichiers
+touch "main.tex", main_tpl
+touch "Makefile", makefile_tpl
+if $options[:graphics?]
+    touch "fig.tex.txt", graphics_sum_tpl
+    Dir.mkdir "fig"
+end
+Dir.mkdir "src" if $options[:listings?]
+Dir.mkdir "inc"
+Dir.chdir "inc"
+touch "packages.tex", packages_tpl
+touch "commands.tex", commands_tpl
+touch "colors.tex", colors_tpl
+touch "lst-conf.tex", listings_tpl if $options[:listings?]
+touch "flyleaf.tex", flyleaf_tpl if $options[:class] == "report"
 
 echo "OK"
 exit
