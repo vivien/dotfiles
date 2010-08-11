e14be0c45c289a4f3fb51b43b5a7609f85a91ab3
Vivien Didelot <vivien.didelot@gmail.com>
2010-08-11 14:12:35 +1000

    renamed README and changed its formatting to markdown

---
+++ README.markdown
@@ -0,0 +1,20 @@
+v0n Dot Files
+=============
+
+Here are most of my config files.
+It includes vim configuration and plugins, my scripts and some Linux config files.
+
+Installation
+------------
+
+If you want to try my config, just clone and deploy with:
+
+    git clone git://github.com/v0n/dotfiles.git ~/.dotfiles
+    cd ~/.dotfiles
+    bash install.sh
+
+This script will prompt you to create/overwrite each file (empty answer is equivalent to no).
+
+You can download and install Inconsolata font with:
+    mkdir -p ~/.fonts && wget http://www.levien.com/type/myfonts/Inconsolata.otf -O ~/.fonts/Inconsolata.otf
+
--- install.sh
+++ install.sh
@@ -5,62 +5,62 @@
 REPLACE_ALL=false
 
 function Overwrite {
-    rm -rf $2
-    ln -sv $1 $2
+  rm -rf $2
+  ln -sv $1 $2
 }
 
 function Deploy {
-    if test -e $2 ; then
-        if $REPLACE_ALL ; then
-            Overwrite $1 $2
-        else
-            read -p "overwrite $2? [Y/n/a] " QUESTION
-            case $QUESTION in
-                'Y'|'y')
-                    Overwrite $1 $2
-                    ;;
-                a)
-                    REPLACE_ALL=true
-                    Overwrite $1 $2
-                    ;;
-                *)
-                    echo "skip $2"
-                    ;;
-            esac
-        fi
+  if test -e $2 ; then
+    if $REPLACE_ALL ; then
+      Overwrite $1 $2
     else
-        read -p "create $2? [Y/n] " QUESTION
-        case $QUESTION in
-            'Y'|'y')
-                test -d `dirname $2` || mkdir -p `dirname $2`
-                ln -sv $1 $2
-                ;;
-            *)
-                echo "skip $2"
-                ;;
-        esac
+      read -p "overwrite $2? [Y/n/a] " QUESTION
+      case $QUESTION in
+        'Y'|'y')
+          Overwrite $1 $2
+          ;;
+        a)
+          REPLACE_ALL=true
+          Overwrite $1 $2
+          ;;
+        *)
+          echo "skip $2"
+          ;;
+      esac
     fi
+  else
+    read -p "create $2? [Y/n] " QUESTION
+    case $QUESTION in
+      'Y'|'y')
+        test -d `dirname $2` || mkdir -p `dirname $2`
+        ln -sv $1 $2
+        ;;
+      *)
+        echo "skip $2"
+        ;;
+    esac
+  fi
 }
 
 for FILE in * ; do
-    SRC=`pwd`/$FILE
-    LINK=$HOME/.$FILE
+  SRC=`pwd`/$FILE
+  LINK=$HOME/.$FILE
 
-    case $FILE in
-        $0|README)
-            ;; # ignore
-        terminator.config)
-            LINK=$HOME/.config/terminator/config
-            Deploy $SRC $LINK
-            ;;
-        bin)
-            LINK=$HOME/$FILE
-            Deploy $SRC $LINK
-            ;;
-        *)
-            Deploy $SRC $LINK
-            ;;
-    esac
+  case $FILE in
+    $0|README.markdown)
+      ;; # ignore
+    terminator.config)
+      LINK=$HOME/.config/terminator/config
+      Deploy $SRC $LINK
+      ;;
+    bin)
+      LINK=$HOME/$FILE
+      Deploy $SRC $LINK
+      ;;
+    *)
+      Deploy $SRC $LINK
+      ;;
+  esac
 done
 
 exit
